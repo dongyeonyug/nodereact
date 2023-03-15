8bbdf50225580d892e2098cf1a918b4f0eaa7878
DONGYEON <yugdongyeon@gmail.com>
2023-03-15 16:49:01 +0900

    secret password

---
+++ config/key.js
@@ -0,0 +1,8 @@
+if(process.env.NODE_ENV==='production'){
+    module.exprots=require('./prod');
+
+}
+
+else{
+    module.exprots=require('./dev');
+}
--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser=require('body-parser');
+
+const config=require('/config/key');
+
+const {User}=require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
 
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://yugdongyeon:qvxzSlkhgmbdttIu@cluster0.6xmdvh3.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI,{
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
+})
 .then(()=>console.log('mongoDB connected')).catch(err=>console.log(err));
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello op')
 })
+
+app.post('/register', (req, res) => {
+
+    //회원 가입 할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+
+    const user=new User(req.body);
+
+    user.save((err,userInfo)=>{
+
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
